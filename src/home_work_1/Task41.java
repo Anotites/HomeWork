0571a17858d631b1e1339278de1d8144e3efe46d
Kseniya Suzdaleva <anotites@gmail.com>
2022-10-29 08:38:36 +0300

    При пересмотре презентации уточнила условие задачи 4.1 и видоизменила ее

--- src/home_work_1/Task41.java
+++ src/home_work_1/Task41.java
@@ -1,19 +1,23 @@
 package home_work_1;
 
-import java.util.Scanner;
-
 public class Task41 {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
 
-        System.out.println("Введите число.");
+        int number1 = (int) (Math.random() * 2147483647 / 2);
+        number1 = number1 * 2;
+        System.out.println(number1 + " - первое число");
+        // Так как в условии было сказано "из двух случайных чисел, одно из которых четное, а второе нечетное",
+        // инициализация чисел выглядит так странно (так как мне нужно было точно знать, что 1 рандомное число
+        // будет четным, а второе нечетным. Без этого условия я могла просто вводить числа из консоли
+        // или взять 2 рандомнымх числа и их уже проверять на четность/нечетность).
+        int number2 = (int) (Math.random() * 2147483647 / 2);
+        number2 = number2 * 2 - 1;
+        System.out.println(number2 + " - второе число");
 
-        int number = console.nextInt();
-
-        if (number % 2 == 1) {
-            System.out.println(number + " - нечетное число");
-        } else {
-            System.out.println(number + " - четное число");
+        if (number1 % 2 == 1) {
+            System.out.println(number1 + " - нечетное число");
+        } else if (number2 % 2 == 1) {
+            System.out.println(number2 + " - нечетное число");
         }
     }
 }
