f22588828fc9941384efeb6a43dd66e89f014ae8
Kseniya Suzdaleva <anotites@gmail.com>
2022-10-29 12:34:17 +0300

    Добавила цикл из классной работы

--- src/home_work_2/TaskFromClass.java
+++ src/home_work_2/TaskFromClass.java
@@ -22,12 +22,12 @@ public class TaskFromClass {
     }
 
     public static void sort(int[] arr) {
-        for (int i = arr.length-1; i > 0; i--) {
+        for (int i = arr.length - 1; i > 0; i--) {
             if (arr[i] < arr[i - 1]) {
                 int temporaryNumber = arr[i];
-                arr[i] = arr[i-1];
-                arr[i-1] = temporaryNumber;
-               }
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temporaryNumber;
+            }
         }
     }
 
