a647889c00cdfab0acb6492fb15b974ea5a7e7c6
Kseniya Suzdaleva <anotites@gmail.com>
2022-11-02 21:35:58 +0300

    Сделала часть домашней работы

--- src/home_work_2/Task11.java
+++ src/home_work_2/Task11.java
@@ -1,2 +1,29 @@
-package home_work_2;public class Task11 {
-}
+package home_work_2;
+
+public class Task11 {
+    public static void main(String[] args) {
+        loop(5);
+        recursion (5);
+        System.out.println(recursion (5));
+    }
+
+    public static void loop(long number) {
+        if (number <= 0) {
+            System.out.println("Введите положительное число как аргумент.");
+        } else {
+            long result = 1;
+            for (long i = 1; i <= number; i++) {
+                result = result * i;
+            }
+            System.out.println(result);
+        }
+    }
+    public static long recursion(long number) {
+        if (number <= 1) {
+            return 1;
+        } else {
+           return number*recursion(number-1);
+            }
+        }
+    }
+
--- src/home_work_2/arrays/Main.java
+++ src/home_work_2/arrays/Main.java
@@ -1,9 +1,48 @@
-package home_work_2.arrays.utils;
+package home_work_2.arrays;
 
-import static home_work_2.arrays.utils.ArraysUtils.arrayFromConsole;
+import static home_work_2.arrays.Task24.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] container = arrayFromConsole();
+//        int[] container1 = arrayFromConsole();
+//        printArray(container1);
+//        System.out.println("");
+
+//        int[] container2 = arrayRandom(5, 100);
+//        printArray(container2);
+//        System.out.println("");
+
+//        printElements();
+//        printEvery2thElement();
+//        printReversedElement();
+
+//        int container3 = sumOfEvenPositiveNumbers();
+//        System.out.print(container3);
+//        System.out.println("");
+
+//        int container4 = MaxNumberWithEvenIndex();
+//        System.out.print(container4);
+//        System.out.println("");
+
+//        int[] container5 = elementsLessThanAverage();
+//        printArray(container5);
+//        System.out.println("");
+
+//        twoMinElements();
+//        System.out.println("");
+
+        int[] container6 = lessArr(10, 90);
+        printArray(container6);
+        System.out.println("");
+
+//        int container7 = sumNumbers();
+//        System.out.print(container7);
+//        System.out.println("");
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
