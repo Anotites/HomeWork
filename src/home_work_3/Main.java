9021a6ca65401e410d5c2cadc3b27f3c06979bfa
Kseniya Suzdaleva <anotites@gmail.com>
2022-11-07 21:32:08 +0300

    Сделала задание 3 из домашней работы Строки.

--- src/home_work_3/Main.java
+++ src/home_work_3/Main.java
@@ -1,10 +1,13 @@
 package home_work_3;
 
+import static home_work_3.Task3.toWeek;
 import static home_work_3.Task4.toHoursMinuteSecondMillisecond;
 
 public class Main {
     public static void main(String[] args) {
         long millisecond = 7403852;
+        int days = 5;
         System.out.println(toHoursMinuteSecondMillisecond(millisecond, true));
+        System.out.println(toWeek(days));
     }
 }
