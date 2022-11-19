f58bda2120942db2c6741a49c64a4c40427f6e7a
Kseniya Suzdaleva <anotites@gmail.com>
2022-11-19 14:58:09 +0300

    В рамках проверки ДЗ на занятии обратила внимание, что мой метод выдавал только результат факториала, а не все выражение, то есть, если вводилось 5, то у меня выдавало 120, а не 1*2*3*4*5=120, поэтому дописала условия. Также невнимательно прочитала условие первоначально, поэтому значение вводилось через аргумент к методу, а не к программе. Это тоже исправила.

--- src/home_work_2/loops/Task11.java
+++ src/home_work_2/loops/Task11.java
@@ -2,47 +2,77 @@ package home_work_2.loops;
 
 public class Task11 {
     public static void main(String[] args) {
-        long number = 5;
-        long numberWithLoop = loop(number);
-        long numberWithRecursion = recursion(number);
-        System.out.println(numberWithLoop);
-        System.out.println(numberWithRecursion);
+        String number = args[0];
+        loop(number);
+        System.out.println(format(number) + " = " + recursion(number));
     }
 
     /**
      * Метод, позволяющий перемножить числа от 1 до числа (включительно), введенного через
      * аргумент к исполняемой программе. Используемый цикл - for.
      *
-     * @param number Число до которого будет производиться перемножение.
-     * @return Произведение чисел от 1 до числа (включительно), введенного через аргумент
-     * к исполняемой программе.
+     * @param number Число, до которого будет производиться перемножение.
      */
-    public static long loop(long number) {
+    public static void loop(String number) {
+        long numberLoop = Long.parseLong(number);
         long result = 1;
-        if (number <= 0) {
+        boolean comma = false;
+        if (numberLoop <= 0) {
             System.out.println("Введите положительное число как аргумент.");
         } else {
-            for (long i = 1; i <= number; i++) {
+            for (long i = 1; i <= numberLoop; i++) {
+                if (comma) {
+                    System.out.print(" * ");
+                } else {
+                    comma = true;
+                }
                 result = result * i;
+                System.out.print(i);
             }
+            System.out.print(" = " + result);
         }
-        return result;
+        System.out.println("");
     }
 
     /**
      * Метод, позволяющий перемножить числа от 1 до числа (включительно), введенного через
      * аргумент к исполняемой программе. Используемый цикл - рекурсия.
      *
-     * @param number Число до которого будет производиться перемножение.
+     * @param number Число, до которого будет производиться перемножение.
      * @return Произведение чисел от 1 до числа (включительно), введенного через аргумент
      * к исполняемой программе.
      */
-    public static long recursion(long number) {
-        if (number <= 1) {
+    public static long recursion(String number) {
+        long numberRecursion = Long.parseLong(number);
+        if (numberRecursion <= 1) {
             return 1;
         } else {
-            return number * recursion(number - 1);
+            String numberRecursion1 = Long.toString(numberRecursion - 1);
+            long numberRecursion2 = recursion(numberRecursion1);
+            return numberRecursion * numberRecursion2;
         }
+    }
+
+    /**
+     * Метод, позволяющий вывести процесс выражения.
+     *
+     * @param number Число, до которого будет производиться перемножение.
+     * @return Отформатированная запись выражения до результата, например, вводим 3, получаем "1*2*3=".
+     */
+    public static String format(String number) {
+        long numberRecursion = Long.parseLong(number);
+        String result = "";
+        boolean comma = false;
+        for (int i = 1; i <= numberRecursion; i++) {
+
+            if (comma) {
+                result += " * ";
+            } else {
+                comma = true;
+            }
+            result += i;
+        }
+        return result;
     }
 }
 
