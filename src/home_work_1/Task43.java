bd06bbdcbf66d3f9b564717668dd69aea1459840
Kseniya Suzdaleva <anotites@gmail.com>
2022-11-28 13:35:03 +0300

    Сделала часть тестов для 1 ДЗ.

--- src/home_work_1/Task43.java
+++ src/home_work_1/Task43.java
@@ -12,16 +12,30 @@ public class Task43 {
         System.out.println("Введите 2 число.");
         int number2 = console.nextInt();
 
-        if (number1 == 0 || number2 == 0) {
-            System.out.println("В данной задаче на ноль делить нельзя. Запустите программу снова, " +
-                    "выбрав другое значение.");
+        System.out.println(divCheck(number1, number2));
+    }
+
+    /**
+     * Метод, который позволяет проверить делимость одного числа на другое.
+     *
+     * @param number1 1 число.
+     * @param number2 2 число.
+     * @return Вывод делится ли одно число на второе без остатка и если нет,
+     * то сообщение о невозможности остаток от их деления.
+     */
+    public static String divCheck(int number1, int number2) {
+        String result;
+        if (number2 == 0) {
+            result = "В данной задаче на ноль делить нельзя. Запустите программу снова, " +
+                    "выбрав другое значение.";
         } else if (number1 % number2 == 0) {
-            System.out.println("1 число без остатка делится на 2 число.");
+            result = "1 число без остатка делится на 2 число.";
         } else {
-            System.out.println("1 число без остатка не делится на 2 число. " +
+            result = "1 число без остатка не делится на 2 число. " +
                     "Их частное равно " + (double) number1 / number2 + ". Остаток от деления " +
-                    "равен " + number1 % number2);
+                    "равен " + number1 % number2+".";
         }
+        return result;
     }
 }
 
---
+++ src/home_work_4/home_work_1/Task43Test.java
@@ -0,0 +1,27 @@
+package home_work_4.home_work_1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static home_work_1.Task43.divCheck;
+
+public class Task43Test {
+
+    @Test
+    public void withoutATraceCheck() {
+        Assertions.assertEquals("1 число без остатка делится на 2 число.", divCheck(32,8));
+    }
+
+    @Test
+    public void withATraceCheck() {
+        Assertions.assertEquals("1 число без остатка не делится на 2 число. " +
+                "Их частное равно 16.5. Остаток от деления равен 1.", divCheck(33,2));
+    }
+
+    @Test
+    public void zeroCheck() {
+        Assertions.assertEquals("В данной задаче на ноль делить нельзя. " +
+                "Запустите программу снова, выбрав другое значение.", divCheck(32,0));
+    }
+
+}
