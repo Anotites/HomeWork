2232f6d2957f40360fa2662bc0ce0540064d287a
Kseniya Suzdaleva <anotites@gmail.com>
2022-12-09 20:08:27 +0300

    Финализировала оформление и документацию для 5 ДЗ.

--- src/home_work_5/api/Comparator.java
+++ src/home_work_5/api/Comparator.java
@@ -4,8 +4,8 @@ public interface Comparator<T> {
     /**
      * Метод для сравнения 2 объектов.
      *
-     * @param item1 1 объект для сравнения.
-     * @param item2 2 объект для сравнения.
+     * @param item1 Первый объект для сравнения.
+     * @param item2 Второй объект для сравнения.
      * @return Реализация сравнения.
      */
     int compare(T item1, T item2);
